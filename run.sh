019201b890f3867a08de549b0bfc860ce72051e5
Marcel Braun <marcel@strg.cc>
2020-07-25 20:45:43 +0200

    Simplify script & install unzip

--- run.sh
+++ run.sh
@@ -2,16 +2,11 @@
 
 if ! [ -x "$(command -v deno)" ]; then
     # Install Deno
-    if [ "$(uname)" == "Darwin" ]; then
-        if [ -x "$(command -v brew)" ]; then
-            # Install Deno via Homebrew
-            brew install deno
-        else
-            # Install Deno via the official install script
-            curl -fsSL https://deno.land/x/install/install.sh | sh
-        fi
+    if [ "$(uname)" == "Darwin" ] && [ -x "$(command -v brew)" ]; then
+        brew install deno
     else
         # Install Deno via the official install script
+        apt --assume-yes install unzip
         curl -fsSL https://deno.land/x/install/install.sh | sh
     fi
 fi
